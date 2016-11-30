64d54e8518b7db498d699e38db9145b0f609c918
Ubuntu <ubuntu@ip-172-31-13-244.us-west-1.compute.internal>
2016-11-30 10:19:10 +0000

    update js

--- client/main.js
+++ client/main.js
@@ -12,30 +12,54 @@ function input () {
         h('input', { 
             type: 'text',
             class: 'list-item'
-
         }),
-        h('button', 'Add')
+        h('button', 
+            { class: 'add-btn' },
+            'Add'
+        )
     ]);
 }
 
 let count = 0; 
 
-let tree = input(); 
+let tree = h('div', 
+    { class: 'list' }, 
+    input()
+); 
+
 //console.log(tree);
 let rootNode = createElement(tree);
 
 document.querySelector('.content').appendChild(rootNode);
+document.querySelector('.add-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    add();
+});
 
 let items = [];
 
 function add() {
     let item = h('li', document.querySelector('list-item').value);
-    items.push(item)
-    let newTree = [input()].concat(h('ul', items));
-     
+    items.push(item);
+    let newTree = h('div', 
+        { class: 'list' }, 
+        [
+            input(),
+            h('ul', items)
+        ]
+    );
+    let patches = diff(tree, newTree);
+    rootNode = patch(rootNode, patches);
+    tree = newTree;
+    console.log('items', items)
+    document.querySelector('.add-btn').addEventListener('click', (e) => {
+        e.preventDefault();
+        add();
+    });
 }
 
 
+/*
 setInterval(function () {
       count++;
 
@@ -44,3 +68,4 @@ setInterval(function () {
       rootNode = patch(rootNode, patches);
       tree = newTree;
 }, 1000);
+*/
