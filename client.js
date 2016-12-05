1d7bea6533cf7f9f16827d786a37d4567618a45a
Casey <leptone55@gmail.com>
2016-12-05 09:12:33 +0000

    trimming

--- client.js
+++ client.js
@@ -3,28 +3,23 @@ import { diff, patch, create as createElement } from 'virtual-dom';
 // let diff = require('virtual-dom').diff;
 // let patch = require('virtual-dom').patch;
 
+let app;
+
 let tree,
     rootNode;
 
-let app,
-    listMaker;
-
 (function intialize() {
-
-    //listMaker = new ListMaker(update);
     app = new App(update);
 
     tree = app.generateNode();
-    //tree = listMaker.generateNode();
     rootNode = createElement(tree);
         
     document.querySelector('.content').appendChild(rootNode);
 })()
 
 function update () {
-    let newTree = app.generateNode()
-    console.log('newTree', newTree)
-    let  patches = diff(tree, newTree);
+    let newTree = app.generateNode(),
+        patches = diff(tree, newTree);
 
     rootNode = patch(rootNode, patches);
     tree = newTree;
